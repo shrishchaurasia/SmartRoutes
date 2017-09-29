d9390df2b483b3a204e5ae597224a973e6d899f2
janoop <anoophky@gmail.com>
2017-09-29 15:25:56 +0530

    das

---
+++ app/src/main/java/com/example/pi/smartroute/AddGpsLocation.java
@@ -0,0 +1,13 @@
+package com.example.pi.smartroute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddGpsLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gps_location);
+    }
+}
--- app/src/main/java/com/example/pi/smartroute/MainActivity.java
+++ app/src/main/java/com/example/pi/smartroute/MainActivity.java
@@ -2,6 +2,17 @@ package com.example.pi.smartroute;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +23,40 @@ public class MainActivity extends AppCompatActivity {
         //dfsdfsdfsdfsf
         //sdsdfsdfsf//
         //sdfsdf
+
+//        LatLng f ;
+//        double b = SphericalUtil.computeDistanceBetween(latLngFrom, latLngTo);
+
+    }
+
+    public void clicked(View view) throws JSONException {
+
+        JSONObject locationJsonObject = new JSONObject();
+        locationJsonObject.put("origin", "54.406505,18.67708");
+        locationJsonObject.put("destination", "54.446251,18.570993");
+        LatlngCalc(locationJsonObject);
+    }
+
+    private void LatlngCalc(JSONObject locationJsonObject) throws JSONException {
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "http://maps.googleapis.com/maps/api/distancematrix/" +
+                "json?origins=" + locationJsonObject.getString("origin") + "&destinations=" + locationJsonObject.getString("destination") + "&mode=driving&" +
+                "language=en-EN&sensor=false";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+//                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                mTextView.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
     }
 }
